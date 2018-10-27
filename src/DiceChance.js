6bb38421663145391643fc7de30017260f582e0e
Cat Chen <catchen@catchen.me>
2018-10-26 22:02:43 -0700

    Created parse function for simplest formula

--- src/DiceChance.js
+++ src/DiceChance.js
@@ -1,3 +1,35 @@
-const DiceChance = {};
+// @flow strict
 
-module.exports = Dice;
+const {
+  DiceAmount,
+  DiceSides,
+  Dices,
+} = require('./Tokens');
+
+const dicePattern = /(\d+)d(\d+)/;
+
+const DiceChance = {
+  parse: function(formula: string): Dices {
+    const matches = formula.match(dicePattern);
+    if (!matches) {
+      throw new Error('Invalid formula.');
+    }
+
+    const [
+      whole,
+      amount,
+      sides,
+    ] = matches;
+
+    if (whole !== formula) {
+      throw new Error('Invalid formula');
+    }
+
+    return new Dices(
+      new DiceAmount(parseInt(amount, 10)),
+      new DiceSides(parseInt(sides, 10)),
+    );
+  },
+};
+
+module.exports = DiceChance;
---
+++ src/Tokens.js
@@ -0,0 +1,33 @@
+// @flow strict
+
+class DiceAmount {
+  value: number;
+
+  constructor(amount: number) {
+    this.value = amount;
+  }
+}
+
+class DiceSides {
+  value: number;
+
+  constructor(sides: number) {
+    this.value = sides;
+  }
+}
+
+class Dices {
+  diceAmount: DiceAmount;
+  diceSides: DiceSides;
+
+  constructor(diceAmount: DiceAmount, diceSides: DiceSides) {
+    this.diceAmount = diceAmount;
+    this.diceSides = diceSides;
+  }
+}
+
+module.exports = {
+  DiceAmount,
+  DiceSides,
+  Dices,
+}
