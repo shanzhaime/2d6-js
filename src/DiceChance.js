1fe856cbbea0d0607941e4fb3edb605618d09dce
Cat Chen <catchen@catchen.me>
2018-10-29 00:10:12 -0700

    Ran Prettier

--- src/DiceChance.js
+++ src/DiceChance.js
@@ -1,10 +1,6 @@
 // @flow strict
 
-const {
-  DiceAmount,
-  DiceSides,
-  Dice,
-} = require('./Tokens');
+const { DiceAmount, DiceSides, Dice } = require("./Tokens");
 
 const dicePattern = /(\d+)d(\d+)/;
 
@@ -12,28 +8,24 @@ const DiceChance = {
   parse: function(formula: string): Dice {
     const matches = formula.match(dicePattern);
     if (!matches) {
-      throw new Error('Invalid formula.');
+      throw new Error("Invalid formula.");
     }
 
-    const [
-      whole,
-      amount,
-      sides,
-    ] = matches;
+    const [whole, amount, sides] = matches;
 
     if (whole !== formula) {
-      throw new Error('Invalid formula');
+      throw new Error("Invalid formula");
     }
 
     return new Dice(
       new DiceAmount(parseInt(amount, 10)),
-      new DiceSides(parseInt(sides, 10)),
+      new DiceSides(parseInt(sides, 10))
     );
   },
 
-  roll: function(formula: string|Dice) {
+  roll: function(formula: string | Dice) {
     let parsedFormula;
-    if (typeof formula === 'string') {
+    if (typeof formula === "string") {
       parsedFormula = DiceChance.parse(formula);
     } else {
       parsedFormula = formula;
@@ -46,14 +38,14 @@ const DiceChance = {
     return result;
   },
 
-  analyze: function(formula: string|Dice) {
+  analyze: function(formula: string | Dice) {
     let parsedFormula;
-    if (typeof formula === 'string') {
+    if (typeof formula === "string") {
       parsedFormula = DiceChance.parse(formula);
     } else {
       parsedFormula = formula;
     }
-  },
+  }
 };
 
 module.exports = DiceChance;
--- src/Tokens.js
+++ src/Tokens.js
@@ -29,5 +29,5 @@ class Dice {
 module.exports = {
   DiceAmount,
   DiceSides,
-  Dice,
-}
+  Dice
+};
