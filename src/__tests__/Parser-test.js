1d72f48560265e148635f1fd0d7712942ddd8e64
Cat Chen <catchen@catchen.me>
2018-11-02 22:17:35 -0700

    Reached 100% test coverage for Parser

--- src/__tests__/Parser-test.js
+++ src/__tests__/Parser-test.js
@@ -11,4 +11,37 @@ it('can parse single dice multiple rolls', () => {
   expect(parsed1D6 instanceof Tokens.Dice);
   expect(parsed1D6.diceAmount.value).toBe(1);
   expect(parsed1D6.diceSides.value).toBe(6);
+
+  const parsed2D6 = Parser.parseFormula(formula2D6);
+  expect(parsed2D6 instanceof Tokens.Dice);
+  expect(parsed2D6.diceAmount.value).toBe(2);
+  expect(parsed2D6.diceSides.value).toBe(6);
+
+  const parsed3D4 = Parser.parseFormula(formula3D4);
+  expect(parsed3D4 instanceof Tokens.Dice);
+  expect(parsed3D4.diceAmount.value).toBe(3);
+  expect(parsed3D4.diceSides.value).toBe(4);
+
+  const parsed4D20 = Parser.parseFormula(formula4D20);
+  expect(parsed4D20 instanceof Tokens.Dice);
+  expect(parsed4D20.diceAmount.value).toBe(4);
+  expect(parsed4D20.diceSides.value).toBe(20);
+});
+
+it('throws if formula is not single dice', () => {
+  expect(() => {
+    Parser.parseFormula('2d');
+  }).toThrow();
+
+  expect(() => {
+    Parser.parseFormula('d6');
+  }).toThrow();
+
+  expect(() => {
+    Parser.parseFormula('42');
+  }).toThrow();
+
+  expect(() => {
+    Parser.parseFormula('2d6+3');
+  }).toThrow();
 });
