60cc480fa2d0f48c3bd752815aa1eb1b1042dfb3
Cat Chen <catchen@catchen.me>
2018-11-03 12:02:52 -0700

    Added tests for v2 DiceChance.roll

--- src/__tests__/DiceChance-test.js
+++ src/__tests__/DiceChance-test.js
@@ -2,6 +2,8 @@ const DiceChance = require('../DiceChance');
 const Tokens = require('../Tokens');
 
 const formula2D6 = '2d6';
+const formula2D6Plus4 = '2d6 + 4';
+const rollTimes = 10000;
 
 it('can parse formula', () => {
   const parsed2D6 = DiceChance.parse(formula2D6);
@@ -9,6 +11,15 @@ it('can parse formula', () => {
   expect(parsed2D6.value instanceof Tokens.Dice);
   expect(parsed2D6.value.diceAmount.value).toBe(2);
   expect(parsed2D6.value.diceSides.value).toBe(6);
+
+  const parsed2D6Plus4 = DiceChance.parse(formula2D6Plus4);
+  expect(parsed2D6Plus4 instanceof Tokens.Formula);
+  expect(parsed2D6Plus4.value instanceof Tokens.Arithmetic);
+  expect(parsed2D6Plus4.value.leftValue instanceof Tokens.Dice);
+  expect(parsed2D6Plus4.value.leftValue.diceAmount.value).toBe(2);
+  expect(parsed2D6Plus4.value.leftValue.diceSides.value).toBe(6);
+  expect(parsed2D6Plus4.value.rightValue).toBe(4);
+  expect(parsed2D6Plus4.value.operator).toBe('+');
 });
 
 it('can roll with a formula', () => {
@@ -16,9 +27,23 @@ it('can roll with a formula', () => {
   expect(rolled2D6).toBeGreaterThanOrEqual(2);
   expect(rolled2D6).toBeLessThanOrEqual(12);
 
-  rolled2D6 = DiceChance.roll(DiceChance.parse(formula2D6));
-  expect(rolled2D6).toBeGreaterThanOrEqual(2);
-  expect(rolled2D6).toBeLessThanOrEqual(12);
+  const parsed2D6 = DiceChance.parse(formula2D6);
+  for (let i = 0; i < rollTimes; i++) {
+    rolled2D6 = DiceChance.roll(parsed2D6);
+    expect(rolled2D6).toBeGreaterThanOrEqual(2);
+    expect(rolled2D6).toBeLessThanOrEqual(12);
+  }
+
+  let rolled2D6Plus4 = DiceChance.roll(formula2D6Plus4);
+  expect(rolled2D6Plus4).toBeGreaterThanOrEqual(6);
+  expect(rolled2D6Plus4).toBeLessThanOrEqual(16);
+
+  const parsed2D6Plus4 = DiceChance.parse(formula2D6Plus4);
+  for (let i = 0; i < rollTimes; i++) {
+    rolled2D6Plus4 = DiceChance.roll(parsed2D6Plus4);
+    expect(rolled2D6Plus4).toBeGreaterThanOrEqual(6);
+    expect(rolled2D6Plus4).toBeLessThanOrEqual(16);
+  }
 });
 
 it('can analyze formula', () => {
